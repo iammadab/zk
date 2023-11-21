ea10e063029af5df43b61671a8c0d483fcf6c6f7
Wisdom Ogwu <iammadab@gmail.com>
2023-11-21 13:34:34 +0100

    implement add poly to transcript

--- src/sumcheck/mod.rs
+++ src/sumcheck/mod.rs
@@ -1,11 +1,14 @@
+use ark_ff::{BigInteger, PrimeField};
 use crate::multilinear_poly::MultiLinearPolynomial;
+use crate::sumcheck::prover::Prover;
+use crate::transcript::Transcript;
 use crate::univariate_poly::UnivariatePolynomial;
 
 pub mod boolean_hypercube;
 mod prover;
 mod verifier;
 
-struct SumcheckProof<F> {
+struct SumcheckProof<F: PrimeField> {
     poly: MultiLinearPolynomial<F>,
     sum: F,
     uni_polys: Vec<UnivariatePolynomial<F>>,
@@ -14,11 +17,46 @@ struct SumcheckProof<F> {
 struct Sumcheck {}
 
 impl Sumcheck {
-    fn prove<F>(poly: MultiLinearPolynomial<F>, sum: F) -> SumcheckProof<F> {
+    fn prove<F: PrimeField>(poly: MultiLinearPolynomial<F>, sum: F) -> SumcheckProof<F> {
+        // let mut challenges = vec![];
+
+        // TODO: how do we add a polynomial to the transcript
+        // for each variable,
         todo!()
     }
 
-    fn verify<F>(proof: SumcheckProof<F>) -> bool {
+    fn verify<F: PrimeField>(proof: SumcheckProof<F>) -> bool {
         todo!()
+    }
+}
+
+/// Add a polynomial to a transcript object
+fn add_poly_to_transcript<F: PrimeField>(poly: &MultiLinearPolynomial<F>, transcript: &mut Transcript) {
+    transcript.append(&poly.n_vars().to_be_bytes());
+    for (var_id, coeff) in poly.coefficients() {
+        transcript.append(&var_id.to_be_bytes());
+        transcript.append(&coeff.into_bigint().to_bytes_be().as_slice());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ark_ff::{Fp64, MontBackend, MontConfig, One};
+    use crate::multilinear_poly::MultiLinearPolynomial;
+    use crate::sumcheck::add_poly_to_transcript;
+    use crate::transcript::Transcript;
+
+    #[derive(MontConfig)]
+    #[modulus = "17"]
+    #[generator = "3"]
+    struct FqConfig;
+    type Fq = Fp64<MontBackend<FqConfig, 1>>;
+
+    #[test]
+    fn test_add_poly_to_transcript() {
+        let mut transcript = Transcript::new();
+        let poly = MultiLinearPolynomial::<Fq>::additive_identity();
+        add_poly_to_transcript(&poly, &mut transcript);
+        assert_eq!(transcript.sample_field_element::<Fq>(), Fq::one());
     }
 }
