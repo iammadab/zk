ca2bd6aaedae96e8a4ac43974a9c4aa649a0badc
Wisdom Ogwu <iammadab@gmail.com>
2023-12-05 18:51:40 +0100

    wip

--- src/gkr/circuit.rs
+++ src/gkr/circuit.rs
@@ -1,5 +1,6 @@
 use ark_ff::PrimeField;
 
+#[derive(Clone)]
 /// Represents a gate wiring (2 inputs 1 output)
 struct Gate {
     out: usize,
@@ -7,6 +8,13 @@ struct Gate {
     in_b: usize,
 }
 
+impl Gate {
+    fn new(out: usize, in_a: usize, in_b: usize) -> Self {
+        Self { out, in_a, in_b }
+    }
+}
+
+#[derive(Clone)]
 /// Holds the add and mul gates in a given layer
 struct Layer {
     add_gates: Vec<Gate>,
@@ -31,12 +39,21 @@ struct Circuit {
 }
 
 impl Circuit {
+    // TODO: implement circuit construction validation
+    fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+}
+
+impl Circuit {
+    /// Evaluate the circuit on a given input
+    // TODO: this doesn't return the input back, decide if returning the input makes sense.
     fn evaluate<F: PrimeField>(&self, input: Vec<F>) -> Result<Vec<Vec<F>>, &'static str> {
         if self.layers.is_empty() {
             return Err("cannot evaluate circuit is empty");
         }
 
-        if self.layers.last().unwrap().len != (2 * input.len()) {
+        if (self.layers.last().unwrap().len * 2) != input.len() {
             return Err("not enough values for input layer");
         }
 
@@ -48,6 +65,7 @@ impl Circuit {
             .rev()
             .map(|layer| {
                 let mut layer_evaluations = vec![F::zero(); layer.len];
+                dbg!(layer.len);
 
                 // add gate evaluations
                 for wire in &layer.add_gates {
@@ -66,5 +84,39 @@ impl Circuit {
                 layer_evaluations
             })
             .collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::gkr::circuit::{Circuit, Gate, Layer};
+
+    use ark_bls12_381::Fr;
+
+    #[test]
+    fn test_circuit_evaluation() {
+        // sample circuit evaluation
+        //      100(*)    - layer 0
+        //     /     \
+        //   5(+)   20(*) - layer 1
+        //   / \    /  \
+        //  2   3   4   5
+
+        // instantiate circuit
+        let layer_0 = Layer::new(vec![], vec![Gate::new(0, 0, 1)]);
+        assert_eq!(layer_0.len, 1);
+
+        let layer_1 = Layer::new(vec![Gate::new(0, 0, 1)], vec![Gate::new(1, 2, 3)]);
+        assert_eq!(layer_1.len, 2);
+
+        let circuit = Circuit::new(vec![layer_0, layer_1]);
+
+        let circuit_eval = circuit
+            .evaluate(vec![Fr::from(2), Fr::from(3), Fr::from(4), Fr::from(5)])
+            .expect("should eval");
+
+        assert_eq!(circuit_eval.len(), 2);
+        assert_eq!(circuit_eval[1], vec![Fr::from(5), Fr::from(20)]);
+        assert_eq!(circuit_eval[0], vec![Fr::from(100)]);
     }
 }
