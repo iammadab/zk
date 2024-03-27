17592676e9c792f5deaf174549c15f00c2a21f5e
Wisdom Ogwu <iammadab@gmail.com>
2024-03-27 06:16:59 +0100

    test prove and verify for sparse merkle tree

--- stark/src/sparse_merkle_tree/merkle.rs
+++ stark/src/sparse_merkle_tree/merkle.rs
@@ -1,6 +1,14 @@
 use crate::hasher::Hasher;
 use crate::sparse_merkle_tree::util::{extra_hash_count, layer_counts, parent, sibling};
 
+/// Represents a sparse merkle proof, need to keep track of the proved node and leaf count
+/// as hint to the verifier
+struct SparseMerkleProof<T> {
+    hashes: Vec<T>,
+    leaf_count: usize,
+    node_index: usize,
+}
+
 /// Sparse Merkle Tree Struct
 /// doesn't pad the leaves to a power of 2, only ensure each layer has an even number of elements
 struct SparseMerkleTree<H: Hasher> {
@@ -41,6 +49,54 @@ impl<H: Hasher> SparseMerkleTree<H> {
         }
 
         SparseMerkleTree { tree, no_of_leaves }
+    }
+
+    /// Generate a merkle proof for the element at a given index
+    fn prove(&self, index: usize) -> Result<SparseMerkleProof<H::Digest>, &'static str> {
+        let mut proof = vec![];
+
+        if index >= self.no_of_leaves {
+            return Err("no element at given proving index");
+        }
+
+        // need to offset the provided index as the leaves are at the end of the tree array
+        let leaf_index = index + extra_hash_count(self.no_of_leaves);
+        let mut proof_node_index = sibling(leaf_index);
+
+        while proof_node_index != 0 {
+            proof.push(self.tree[proof_node_index].clone());
+            proof_node_index = sibling(parent(proof_node_index, layer_counts(self.no_of_leaves)));
+        }
+
+        Ok(SparseMerkleProof {
+            hashes: proof,
+            node_index: leaf_index,
+            leaf_count: self.no_of_leaves,
+        })
+    }
+
+    /// Verify the merkle proof for a given leaf element
+    fn verify(
+        input: &H::Item,
+        proof: SparseMerkleProof<H::Digest>,
+        expected_root_hash: &H::Digest,
+    ) -> bool {
+        let input_hash = H::hash_item(input);
+
+        // this represents the node index of the current running hash
+        let mut known_hash_index = proof.node_index;
+
+        let root_hash = proof.hashes.iter().fold(input_hash, |acc, proof_hash| {
+            let next_known_hash = if known_hash_index % 2 == 0 {
+                H::hash_digest_slice(&[proof_hash, &acc])
+            } else {
+                H::hash_digest_slice(&[&acc, proof_hash])
+            };
+            known_hash_index = parent(known_hash_index, layer_counts(proof.leaf_count));
+            next_known_hash
+        });
+
+        root_hash == *expected_root_hash
     }
 }
 
@@ -117,5 +173,48 @@ mod tests {
         let hash_root = Sha3Hasher::hash_digest_slice(&[&hash_2_4_6_2, &hash_4_empty_default]);
         assert_eq!(tree.tree[0], hash_root);
         assert_eq!(tree.root_hash(), &hash_root);
+    }
+
+    #[test]
+    fn test_prove_verify_index() {
+        let values = vec![
+            2_u8.to_be_bytes().to_vec(),
+            4_u8.to_be_bytes().to_vec(),
+            6_u8.to_be_bytes().to_vec(),
+            2_u8.to_be_bytes().to_vec(),
+            4_u8.to_be_bytes().to_vec(),
+            vec![],
+        ];
+
+        let tree = build_merkle_tree();
+
+        // cannot prove outside of allow leaf indexes
+        assert_eq!(tree.prove(6).is_err(), true);
+
+        let proof = tree.prove(0).unwrap();
+        assert_eq!(proof.hashes.len(), 3);
+        assert_eq!(proof.node_index, 7);
+        assert_eq!(proof.leaf_count, 6);
+        assert_eq!(proof.hashes[0], tree.tree[8]);
+        assert_eq!(proof.hashes[1], tree.tree[4]);
+        assert_eq!(proof.hashes[2], tree.tree[2]);
+        assert!(SparseMerkleTree::<Sha3Hasher>::verify(
+            &values[0],
+            proof,
+            tree.root_hash()
+        ));
+
+        let proof = tree.prove(3).unwrap();
+        assert_eq!(proof.hashes.len(), 3);
+        assert_eq!(proof.node_index, 10);
+        assert_eq!(proof.leaf_count, 6);
+        assert_eq!(proof.hashes[0], tree.tree[9]);
+        assert_eq!(proof.hashes[1], tree.tree[3]);
+        assert_eq!(proof.hashes[2], tree.tree[2]);
+        assert!(SparseMerkleTree::<Sha3Hasher>::verify(
+            &values[3],
+            proof,
+            tree.root_hash()
+        ));
     }
 }
