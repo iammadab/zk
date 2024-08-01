bbb7482bdc0cdef08768293b8ea62fff3ad2c42a
Wisdom Ogwu <iammadab@gmail.com>
2024-08-01 10:00:11 +0100

    implement evaluate for product poly

--- polynomial/src/product_poly.rs
+++ polynomial/src/product_poly.rs
@@ -31,6 +31,18 @@ impl<F: PrimeField> ProductPoly<F> {
             polynomials,
         })
     }
+
+    /// Evaluate the product poly using the following
+    /// P(x) = A(x).B(x).C(x)
+    pub fn evaluate(&self, assignments: &[F]) -> Result<F, &'static str> {
+        if assignments.len() != self.n_vars {
+            return Err("evaluate must assign to all variables");
+        }
+
+        self.polynomials.iter().try_fold(F::one(), |product, poly| {
+            poly.evaluate(assignments).map(|value| product * value)
+        })
+    }
 }
 
 #[cfg(test)]
@@ -63,5 +75,36 @@ mod tests {
         .unwrap();
         let prod_poly = ProductPoly::new(vec![mle_a, mle_b]);
         assert_eq!(prod_poly.is_err(), true);
+    }
+
+    #[test]
+    fn test_evaluate() {
+        // create prod_poly from mle's with the same number of variables
+        let mle_a = MultiLinearPolynomial::new(
+            2,
+            vec![Fr::from(2), Fr::from(8), Fr::from(10), Fr::from(14)],
+        )
+        .unwrap();
+        let mle_b = MultiLinearPolynomial::new(
+            2,
+            vec![Fr::from(2), Fr::from(8), Fr::from(10), Fr::from(22)],
+        )
+        .unwrap();
+        let mle_c = MultiLinearPolynomial::new(
+            2,
+            vec![Fr::from(2), Fr::from(8), Fr::from(10), Fr::from(22)],
+        )
+        .unwrap();
+
+        let direct_product = mle_a.evaluate(&[Fr::from(1), Fr::from(10)]).unwrap()
+            * mle_b.evaluate(&[Fr::from(1), Fr::from(10)]).unwrap()
+            * mle_c.evaluate(&[Fr::from(1), Fr::from(10)]).unwrap();
+
+        let prod_poly = ProductPoly::new(vec![mle_a, mle_b, mle_c]).unwrap();
+
+        assert_eq!(
+            prod_poly.evaluate(&[Fr::from(1), Fr::from(10)]).unwrap(),
+            direct_product
+        );
     }
 }
