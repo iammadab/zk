c8c15d664568e7cec7791f8d8ec5815354dc3541
Wisdom Ogwu <iammadab@gmail.com>
2024-08-01 15:32:28 +0100

    add mulitivariate sumcheck test

--- sumcheck2/src/lib.rs
+++ sumcheck2/src/lib.rs
@@ -3,6 +3,7 @@ pub mod verifier;
 
 use ark_ff::{BigInteger, PrimeField};
 
+#[derive(Debug)]
 /// Holds the round polys and the initial prover claimed sum for sumcheck
 pub struct SumcheckProof<F: PrimeField> {
     sum: F,
@@ -50,15 +51,53 @@ mod tests {
     }
 
     #[test]
-    fn test_sumcheck_correct_sum() {
-        // non-multivariate case
+    fn test_sumcheck_correct_sum_multilinear() {
+        // p = 2ab + 3bc
         let p = p_2ab_3bc();
         let prod_poly = ProductPoly::new(vec![p]).unwrap();
         let proof = SumcheckProver::<1, Fr>::prove(prod_poly.clone(), Fr::from(10)).unwrap();
         let verification_result =
             SumcheckVerifier::verify(prod_poly, proof).expect("proof is invalid");
         assert!(verification_result);
+    }
 
-        // TODO: test multivariate case
+    #[test]
+    fn test_correct_sum_multivariate_deg_2() {
+        // p = 2a^2b + 3ab
+        // decomposed into
+        // p1 = (2a + 0b + 3)
+        // p2 = (ab)
+        // p = p1 . p2
+
+        // p1 = (2a + 0b + 3)
+        let p1 = MultiLinearPolynomial::new(
+            2,
+            CoeffMultilinearPolynomial::new(
+                2,
+                vec![
+                    (Fr::from(2), vec![true, false]),
+                    (Fr::from(0), vec![false, true]),
+                    (Fr::from(3), vec![false, false]),
+                ],
+            )
+            .unwrap()
+            .to_evaluation_form(),
+        )
+        .unwrap();
+
+        // p2 = (ab)
+        let p2 = MultiLinearPolynomial::new(
+            2,
+            CoeffMultilinearPolynomial::new(2, vec![(Fr::from(1), vec![true, true])])
+                .unwrap()
+                .to_evaluation_form(),
+        )
+        .unwrap();
+
+        let p = ProductPoly::new(vec![p1, p2]).unwrap();
+
+        let proof = SumcheckProver::<2, Fr>::prove(p.clone(), Fr::from(5)).unwrap();
+        let verification_result = SumcheckVerifier::verify(p, proof).expect("proof is invalid");
+        assert!(verification_result);
     }
 }
