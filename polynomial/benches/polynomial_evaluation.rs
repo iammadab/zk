f79782d0e5a4dd33cbb78cae4484e4f9cb42d2d0
Wisdom Ogwu <iammadab@gmail.com>
2024-12-23 12:11:06 +0100

    add benchmark for pairing index

--- polynomial/benches/polynomial_evaluation.rs
+++ polynomial/benches/polynomial_evaluation.rs
@@ -3,7 +3,11 @@ use ark_ff::PrimeField;
 use ark_poly::{DenseMultilinearExtension, MultilinearExtension};
 use ark_std::test_rng;
 use criterion::{criterion_group, criterion_main, Criterion};
+use field_tracker::{end_tscope, print_summary, start_tscope, Ft};
 use polynomial::multilinear::evaluation_form::MultiLinearPolynomial;
+
+/// Operation trackable field type
+type FTr = Ft!(Fr);
 
 pub fn n_points<F: PrimeField>(n: usize) -> Vec<F> {
     let mut rng = test_rng();
@@ -54,6 +58,30 @@ pub fn arkworks_benchmark(c: &mut Criterion) {
     });
 }
 
+pub fn arkworks_field_op_benchmark(_c: &mut Criterion) {
+    start_tscope!("arkworks_evaluate");
+    start_tscope!("poly_eval 18var");
+    let (poly, to_eval) = ark_random_poly_evaluation_pair::<FTr>(18);
+    poly.fix_variables(&to_eval.as_slice());
+    end_tscope!();
+
+    start_tscope!("poly_eval 19var");
+    let (poly, to_eval) = ark_random_poly_evaluation_pair::<FTr>(19);
+    poly.fix_variables(&to_eval.as_slice());
+    end_tscope!();
+
+    start_tscope!("poly_eval 20var");
+    let (poly, to_eval) = ark_random_poly_evaluation_pair::<FTr>(20);
+    poly.fix_variables(&to_eval.as_slice());
+    end_tscope!();
+
+    start_tscope!("poly_eval 21var");
+    let (poly, to_eval) = ark_random_poly_evaluation_pair::<FTr>(21);
+    poly.fix_variables(&to_eval.as_slice());
+    end_tscope!();
+    end_tscope!();
+}
+
 pub fn poly_eval_benchmark(c: &mut Criterion) {
     c.bench_function("evaluate_18_vars", |b| {
         let (poly, to_eval) = poly_eval_pair::<Fr>(18);
@@ -76,6 +104,38 @@ pub fn poly_eval_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, poly_eval_benchmark, arkworks_benchmark,);
+pub fn poly_field_op_benchmark(_c: &mut Criterion) {
+    start_tscope!("poly_evaluate");
+    start_tscope!("poly_eval 18var");
+    let (poly, to_eval) = poly_eval_pair::<FTr>(18);
+    poly.evaluate(&to_eval.as_slice());
+    end_tscope!();
+
+    start_tscope!("poly_eval 19var");
+    let (poly, to_eval) = poly_eval_pair::<FTr>(19);
+    poly.evaluate(&to_eval.as_slice());
+    end_tscope!();
+
+    start_tscope!("poly_eval 20var");
+    let (poly, to_eval) = poly_eval_pair::<FTr>(20);
+    poly.evaluate(&to_eval.as_slice());
+    end_tscope!();
+
+    start_tscope!("poly_eval 21var");
+    let (poly, to_eval) = poly_eval_pair::<FTr>(21);
+    poly.evaluate(&to_eval.as_slice());
+    end_tscope!();
+    end_tscope!();
+
+    print_summary!();
+}
+
+criterion_group!(
+    benches,
+    arkworks_field_op_benchmark,
+    poly_field_op_benchmark,
+    poly_eval_benchmark,
+    arkworks_benchmark
+);
 
 criterion_main!(benches);
