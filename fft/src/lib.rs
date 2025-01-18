d681643d5e29d9d23234b9ae11512c4e0d7175f0
Wisdom Ogwu <iammadab@gmail.com>
2025-01-18 17:21:10 +0100

    sketch implementation

--- fft/src/lib.rs
+++ fft/src/lib.rs
@@ -1,9 +1,40 @@
 use ark_ff::FftField;
 
+// TODO: add documentation
 pub fn fft<F: FftField>(coefficients: Vec<F>) -> Vec<F> {
+    if coefficients.len() == 1 {
+        return coefficients;
+    }
+
+    if !coefficients.len().is_power_of_two() {
+        panic!("coefficients should be a power of 2");
+    }
+
+    let n = coefficients.len();
+
+    let (even, odd) = split_even_odd(coefficients);
+
+    let even_evaluations = fft(even);
+    let odd_evaluations = fft(odd);
+
+    // n-th root of unity
+    let omega = F::get_root_of_unity(n as u64);
+
+    // recombination step
+    let mut evaluations = vec![F::ZERO; n];
+    for i in 0..n / 2 {
+        evaluations[i] = even_evaluations[i] + odd_evaluations[i];
+        evaluations[i + n / 2] = even_evaluations[i] - odd_evaluations[i];
+    }
+
+    evaluations
+}
+
+// TODO: add documentation
+pub fn ifft<F: FftField>(evaluations: Vec<F>) -> Vec<F> {
     todo!()
 }
 
-pub fn ifft<F: FftField>(evaluations: Vec<F>) -> Vec<F> {
+fn split_even_odd<T>(data: Vec<T>) -> (Vec<T>, Vec<T>) {
     todo!()
 }
