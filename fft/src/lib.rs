3303aba9dd0904f7d37bed5727999c28a0e1da77
Wisdom Ogwu <iammadab@gmail.com>
2025-01-18 16:59:55 +0100

    define function signature

--- fft/src/lib.rs
+++ fft/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use ark_ff::FftField;
+
+pub fn fft<F: FftField>(coefficients: Vec<F>) -> Vec<F> {
+    todo!()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn ifft<F: FftField>(evaluations: Vec<F>) -> Vec<F> {
+    todo!()
 }
