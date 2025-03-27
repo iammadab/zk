301e40d4f7ac0e8f07049d0c89a674a030bce810
Wisdom Ogwu <iammadab@gmail.com>
2025-03-27 17:47:19 +0100

    basic implementation of the eq table

--- libra/src/lib.rs
+++ libra/src/lib.rs
@@ -1,2 +1,13 @@
-// beginning of libra work
-// take transcript into sumcheck prover
+use ark_ff::PrimeField;
+use polynomial::multilinear::pairing_index::index_pair;
+
+fn eq_table<F: PrimeField>(r: &[F]) -> Vec<F> {
+    let mut result = vec![F::one(); 1 << r.len()];
+    for (i, val) in r.iter().enumerate() {
+        for (l, r) in index_pair(r.len() as u8, i as u8) {
+            result[l] *= F::one() - val;
+            result[r] *= val;
+        }
+    }
+    result
+}
