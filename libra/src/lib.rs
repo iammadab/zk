a63904aef1f0f9fd128f10a0e1d659df106f58d6
Wisdom Ogwu <iammadab@gmail.com>
2025-03-27 17:52:21 +0100

    implement naive eq table implementation

--- libra/src/lib.rs
+++ libra/src/lib.rs
@@ -11,3 +11,18 @@ fn eq_table<F: PrimeField>(r: &[F]) -> Vec<F> {
     }
     result
 }
+
+#[cfg(test)]
+mod test {
+    use crate::eq_table;
+    use ark_bn254::Fr;
+
+    #[test]
+    fn test_eq_table_generation() {
+        assert_eq!(eq_table(&[Fr::from(3)]), vec![Fr::from(-2), Fr::from(3)]);
+        assert_eq!(
+            eq_table(&[Fr::from(3), Fr::from(5)]),
+            vec![Fr::from(8), Fr::from(-10), Fr::from(-12), Fr::from(15)]
+        );
+    }
+}
