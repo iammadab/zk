13d8bec252a3c138a74a8607cd7cc2280c9e9b18
Wisdom Ogwu <iammadab@gmail.com>
2025-03-29 10:36:37 +0100

    basic ideas gotten for prover implementation

--- libra/src/prover.rs
+++ libra/src/prover.rs
@@ -1,6 +1,6 @@
 use crate::{
     circuit::layered_circuit::LayeredCircuit,
-    util::{eq_table, phase_one_table, w_i},
+    util::{eq_table, phase_one_table, phase_two_table, w_i},
 };
 use ark_ff::PrimeField;
 use polynomial::{multilinear::evaluation_form::MultiLinearPolynomial, product_poly::ProductPoly};
@@ -49,16 +49,44 @@ fn libra_sumcheck<F: PrimeField>(
     // phase 1
     // first we need to construct the phase 1 table then run sumcheck product
     let i_gz = eq_table(output_challenges);
-    let hg_table = phase_one_table(&muli, &i_gz, w_next.evaluation_slice());
+    let hg_table = phase_one_table(muli, &i_gz, w_next.evaluation_slice());
     let phase_one_product_poly = ProductPoly::new(vec![
         MultiLinearPolynomial::new_with_pad(hg_table, None),
-        w_next,
+        w_next.clone(),
     ])
     .unwrap();
 
     let (phase_one_proof, u_challenges) =
         SumcheckProver::<2, F>::prove_partial(phase_one_product_poly, F::zero(), transcript)
             .unwrap();
+
+    // phase 2
+    let i_ux = eq_table(&u_challenges);
+    let f_2_u = w_next.evaluate(&u_challenges).unwrap();
+    // TODO: implement scalar mul for multilinear polynomials
+    let f_3 = w_next
+        .evaluation_slice()
+        .iter()
+        .cloned()
+        .map(|v| v * f_2_u)
+        .collect::<Vec<_>>();
+    let f1_table = phase_two_table(muli, &i_gz, &i_ux);
+    let phase_two_product_poly = ProductPoly::new(vec![
+        MultiLinearPolynomial::new_with_pad(f_3, None),
+        MultiLinearPolynomial::new_with_pad(f1_table, None),
+    ])
+    .unwrap();
+
+    let (phase_two_proof, v_challenges) =
+        SumcheckProver::<2, F>::prove_partial(phase_two_product_poly, F::zero(), transcript)
+            .unwrap();
+
+    // merge the proofs and merge the challenges
+    // do this for all the two addi's also, before I can do this
+    // I need to implement sumcheck for vec of product poly
+    // so I can run for the three equations in lock step
+    // do phase 1 for all 3 then do phase 2 for all 3
+    // finally merge the proofs from the two stages
 
     todo!()
 }
