087f368e4dd93fcb80472ee7b434c761373552e4
Wisdom Ogwu <iammadab@gmail.com>
2025-03-29 08:41:32 +0100

    wip

--- libra/src/prover.rs
+++ libra/src/prover.rs
@@ -1,7 +1,12 @@
+use crate::circuit::layered_circuit::LayeredCircuit;
 use ark_ff::PrimeField;
 use sumcheck::SumcheckProof;
 
 struct GKRProof<F: PrimeField> {
     sumcheck_proofs: Vec<SumcheckProof<F>>,
     prover_hints: Vec<[F; 2]>,
+}
+
+fn prove<F: PrimeField>(circuit: &LayeredCircuit, evaluations: Vec<Vec<F>>) -> GKRProof<F> {
+    todo!()
 }
