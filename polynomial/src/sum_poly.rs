3294817ef489bbc9774eb90101ee32af3dd600b5
Wisdom Ogwu <iammadab@gmail.com>
2025-03-29 16:42:09 +0100

    implement new for sum poly

--- polynomial/src/sum_poly.rs
+++ polynomial/src/sum_poly.rs
@@ -11,7 +11,23 @@ pub struct SumPoly<F: PrimeField> {
 
 impl<F: PrimeField> SumPoly<F> {
     pub fn new(polynomials: Vec<ProductPoly<F>>) -> Result<Self, &'static str> {
-        todo!()
+        if polynomials.is_empty() {
+            return Err("cannot create sum poly from empty polynomials");
+        }
+
+        // ensure all the product polynomials have the same number of variables
+        let expected_num_of_vars = polynomials[0].n_vars();
+        let equal_variables = polynomials
+            .iter()
+            .all(|poly| poly.n_vars() == expected_num_of_vars);
+        if !equal_variables {
+            return Err("all product polys should have the same number of variables");
+        }
+
+        Ok(Self {
+            n_vars: expected_num_of_vars,
+            polynomials,
+        })
     }
 
     pub fn evaluate(&self, assignments: &[F]) -> Result<F, &'static str> {
